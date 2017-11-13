f4578372d7759b3698a734d8bde2564cb39ae719
evanmanz <evanmanzagol@gmail.com>
2017-11-13 13:28:19 -0500

    Created placeholder variables
    Add variables for the map and the player as placeholders for later.

--- CIS150_MineMaze/CIS150_MineMaze/CIS150_MineMaze.cpp
+++ CIS150_MineMaze/CIS150_MineMaze/CIS150_MineMaze.cpp
@@ -2,10 +2,50 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void displayStartup() {
+	cout << "======================================================================================================================" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "                                                   Mine Maze                                                          " << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "======================================================================================================================" << endl;
+}
+
 
 
 int main()
 {
+
+	// ======================================
+	// Map variables
+	// ======================================
+	string easyLevel = ""; // Placeholders for later.
+	// 10 x 10 Grid
+	string mediumLevel = "";
+	// 15 x 15 Grid
+	string hardLevel = "";
+	// 25 x 25 Grid
+
+	// ======================================
+	// Player variables
+	// ======================================
+	int lives; // Placeholder for later, will depend on the level.
+	int moves; // Placeholder for later, limited amount of moves depending on the level.
+
+
+	displayStartup();
+	
+
+
+	system("pause");
     return 0;
 }
 
